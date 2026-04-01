15d33ed33dc4fc41289f5d243559a8276e877f0d
Daniel Kesselberg <mail@danielkesselberg.de>
2026-04-01 12:04:55 +0200

    docs: setContentTypeParameter accepts string|null as data
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- lib/Horde/Mime/Part.php
+++ lib/Horde/Mime/Part.php
@@ -791,10 +791,10 @@ implements ArrayAccess, Countable, RecursiveIterator, Serializable
      * Add/remove a content type parameter to this part.
      *
      * @param string $label  The content-type parameter label.
-     * @param string $data   The content-type parameter data. If null, removes
+     * @param ?string $data  The content-type parameter data. If null, removes
      *                       the parameter (@since 2.8.0).
      */
-    public function setContentTypeParameter($label, $data)
+    public function setContentTypeParameter($label, $data): void
     {
         $ct = $this->_headers['content-type'];
 
